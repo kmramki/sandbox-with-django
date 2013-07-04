2a0e232aeb8d4fd85c6daa3e67c5924d1872312a
Ramki Kazhiyur-Mannar <kmramki@gmail.com>
2013-07-04 02:35:00 -0700

    Made install compatible with Mac.

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+[ "`uname -s`" == "Linux" ] ; IS_LINUX=$?
+
 curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py
 python virtualenv.py .env
 . .env/bin/activate
-
+ 
 pip install django
 pip install ujson
 pip install pexpect
 pip install ipython
-pip install python-mysql
+
+if [ $IS_LINUX -eq 1 ]
+then
+	 pip install mysql-python
+else
+	 pip install python-mysql
+fi
 
